fcc02c862fe64b6d137f1cae8534fa65d7f834e8
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-04-09 21:26:05 +0200

    configure dotenv, add db credentials to .env file

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
 require 'rails/all'
+Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+HOSTNAME = ENV['HOSTNAME']
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,5 +18,7 @@ module YallaNotlob
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
   end
 end
