74b1d8ff1185de726db2372c5fbbd87bc7958b97
AymanMagdy <magdia883@gmail.com>
2020-04-12 17:36:04 +0200

    valid passwrod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,37 @@ class UsersController < ApplicationController
     end
 
     def signup
+        user_full_name =params['user_full_name'].to_s()
+        user_email = params['user_email_address'].to_s()
+        user_password = params['user_password'].to_s()
+        user_confirmation_password = params['user_confirmation_password'].to_s()
+
+        if valid_email(user_email) && valid_password(user_password, conf_password)
+            puts "Done sign up!"
+        else
+            puts "Error, signing up!"
+        end
     end
 
     def groups
+    end
+
+    def valid_email(email)
+        @email = email
+        if email.match(/\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+            return true;
+        else
+            return false;
+        end
+    end
+
+    def valid_password(password, conf_password)
+        @password = password
+        @conf_password = conf_password
+        if (@password == @conf_password)
+            return true
+        else
+            return false
+        end
     end
 end
