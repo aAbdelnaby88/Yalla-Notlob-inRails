27f283c8ef0af5db2e3c21785c2c3630db520aaa
AymanMagdy <magdia883@gmail.com>
2020-04-12 19:31:23 +0200

    done sign up method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,31 +2,42 @@ class UsersController < ApplicationController
     def signin
     end
 
+    # Sign up users.
     def signup
-        user_full_name =params['user_full_name'].to_s()
+        user_full_name = params['user_full_name'].to_s()
         user_email = params['user_email_address'].to_s()
         user_password = params['user_password'].to_s()
         user_confirmation_password = params['user_confirmation_password'].to_s()
 
-        if valid_email(user_email) && valid_password(user_password, conf_password)
-            puts "Done sign up!"
+        # checking the vlidation of the email and the password.
+        if valid_email(user_email) && valid_password(user_password, user_confirmation_password)
+            User.create(name: user_full_name, email: user_email, password: user_password)
+            render 'users/signed_up_success'
         else
-            puts "Error, signing up!"
+            render 'users/signed_up_error'
         end
     end
 
     def groups
     end
 
-    def valid_email(email)
-        @email = email
-        if email.match(/\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-            return true;
-        else
+    # validating the email(regex for the email, and if the email exists.)
+    def valid_email(user_email)
+        @user_email = user_email
+        if @user_email.match(/\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+            is_user_exist = User.where(email: @user_email)
+            # if returned an object from the database.
+            if is_user_exist.length > 0
+                return false;
+            else # if there is no returned object.
+                return true
+            end
+        else # if the email is not a valid email.
             return false;
         end
     end
 
+    # validating the password if they're matching.
     def valid_password(password, conf_password)
         @password = password
         @conf_password = conf_password
