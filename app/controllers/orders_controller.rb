2fa57b726e03126843f7b1397f52ab6bddbb7f1c
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-04-13 05:13:55 +0200

    add s3 upload feature

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
+require 'aws-sdk-s3' 
+require 'securerandom'
+
 class OrdersController < ApplicationController
+
+    def upload_image_to_s3
+        file_name = SecureRandom.urlsafe_base64
+        upload_file = '/home/muhammad/Downloads/32.png'
+
+        s3 = Aws::S3::Resource.new
+        obj = s3.bucket('yalla-notlob').object(file_name)
+        obj.upload_file(upload_file)
+
+        url = obj.public_url
+    end
 end
