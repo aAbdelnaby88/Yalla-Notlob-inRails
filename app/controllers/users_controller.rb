cd0457d89d26def903213d3ad297a3e5c6d5443a
AymanMagdy <magdia883@gmail.com>
2020-04-14 02:54:16 +0200

    getting the error of the sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    
     def signin_form
         user_email = params[:user_email]
         password = params[:user_password]
@@ -26,12 +25,18 @@ class UsersController < ApplicationController
         user_confirmation_password = params['user_confirmation_password'].to_s()
 
         # checking the vlidation of the email and the password and if true create a new user.
-        if valid_email(user_email) && valid_password(user_password, user_confirmation_password)
-            User.create(name: user_full_name, email: user_email, password: user_password)
-            render 'users/signed_up_success'
-            return 
+        if ((user_password == user_confirmation_password) != nil)
+
+            @is_done = User.create(name: user_full_name, email: user_email, password: user_password)
+            if @is_done.errors.any?
+                @is_done.errors.full_messages.each do |msg|
+                    puts "#{msg}"
+                end
+                render 'users/signed_up_error'
+            else
+                render 'users/signed_up_success'
+            end 
         else
-            puts "Error from the validating email"
             render 'users/signed_up_error'
         end
     end
@@ -40,29 +45,29 @@ class UsersController < ApplicationController
     end
 
     # validating the email(regex for the email, and if the email exists.)
-    def valid_email(user_email)
-        @user_email = user_email
-        if @user_email.match(/\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-            is_user_exist = User.where(email: @user_email)
-            # if returned an object from the database.
-            if is_user_exist.length > 0
-                return false;
-            else # if there is no returned object.
-                return true
-            end
-        else # if the email is not a valid email.
-            return false;
-        end
-    end
+    # def valid_email(user_email)
+    #     @user_email = user_email
+    #     if @user_email.match(/\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+    #         is_user_exist = User.where(email: @user_email)
+    #         # if returned an object from the database.
+    #         if is_user_exist.length > 0
+    #             return false;
+    #         else # if there is no returned object.
+    #             return true
+    #         end
+    #     else # if the email is not a valid email.
+    #         return false;
+    #     end
+    # end
 
-    # validating the password if they're matching.
-    def valid_password(password, conf_password)
-        @password = password
-        @conf_password = conf_password
-        if (@password == @conf_password)
-            return true
-        else
-            return false
-        end
-    end
+    # # validating the password if they're matching.
+    # def valid_password(password, conf_password)
+    #     @password = password
+    #     @conf_password = conf_password
+    #     if (@password == @conf_password)
+    #         return true
+    #     else
+    #         return false
+    #     end
+    # end
 end
